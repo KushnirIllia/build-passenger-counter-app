9cf95bfbeeca04dc8d27f8e1e573381cfcec941c
Illia Kushnir <illiakushnir@illia-Kushnirs-MacBook-Pro.local>
2022-12-07 18:27:55 +0200

    Got control btns by ids. Set counter Add function increment, save, decrement, reset.

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+	count += 1;
+	countEl.textContent = count;
+}
+
+function save() {
+	if (count === 0) {
+		return;
+	}
+	let countStr = " " + count + " - ";
+	saveEl.textContent += countStr;
+	count = 0;
+	countEl.textContent = 0;
+}
+function decrement() {
+	if (count <= 0) {
+		return;
+	}
+	count -= 1;
+	countEl.innerText = count;
+}
+function reset() {
+	count = 0;
+	countEl.innerText = count;
+}
